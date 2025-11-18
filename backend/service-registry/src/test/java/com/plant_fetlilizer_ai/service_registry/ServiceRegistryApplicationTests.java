73c1b56b361d79112c3c4c4537df248cb97ad07b
somnath-more <somnathmore8383@gmail.com>
2025-11-18 21:58:46 +0530

    feat: Initialize auth-service,api-gateway and service-registry modules with Spring Boot

---
+++ backend/service-registry/src/test/java/com/plant_fetlilizer_ai/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plant_fetlilizer_ai.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
